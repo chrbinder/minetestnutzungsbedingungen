5a349cb06bcca499844a078fc177640ccf02ec70
TEbinger <thomas.ebinger@gmx.de>
2020-07-03 21:17:30 +0200

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,24 +1,18 @@
 # Based on https://github.com/ChaosWormz/mt_terms_of_use
 
 local RULES = [[
-Here are the rules:
-1)	Don't attempt to crash, lag, damage, disable, exploit or disturb the server or game.
-2)	Don't ruin creations not owned by yourself.
-3)	Don't be harrassful. This includes (but is not limited to) asking for stuff constantly, being rude, being inconsiderate or spamming the chat.
-4)	Feel free to walk around wherever, but if the area is clearly private stay out!
-5)	Don't take items from other's chests unless otherwise stated.
-6)	Don't take part in PVP (punching other players) unless all players taking part agree.
-7)  Multiple accounts are dissalowed and you are responsible for looking after your account and login details.
-8)	We want to make this world look as nice as possible, so please avoid building 1x1 towers or tunnels and placing random blocks everywhere. A few cool buildings would be nice too :)
-Click the accept button and you will be able to build.
-Thanks :)
+Nutzungsbedingungen
+1) Ich bin damit einverstanden, dass Daten, die notwendig sind für das Spiel, auf dem Server gespeichert werden (Gebaute und abgebaute Elemente sowie Äußerungen im Chat)
+2) Ich werde nichts mutwillig zerstören, was andere gebaut haben. 
+Auf Bestätigen klicken, um Baurechte zu erhalten.
+Danke 
 ]]
 
 local function make_formspec()
 	local size = { "size[10,8]" }
-	table.insert(size, "textarea[0.5,0.5;9.5,8;TOS;Here are the rules. Click accept if you agree with them;"..RULES.."]")
-	table.insert(size, "button_exit[6,7.4;1.5,0.5;accept;I Agree :)]")
-	table.insert(size, "button[7.5,7.4;1.5,0.5;decline;I Disagree :(]")
+	table.insert(size, "textarea[0.5,0.5;9.5,8;TOS;Das sind die Nutzungsbedingungen. Bitte klicken, um sie zu bestätigen;"..RULES.."]")
+	table.insert(size, "button_exit[6,7.4;1.5,0.5;accept;JA]")
+	table.insert(size, "button[7.5,7.4;1.5,0.5;decline;NEIN]")
 	return table.concat(size)
 end
 
@@ -27,23 +21,22 @@ minetest.register_on_player_receive_fields(function(player, formname, fields)
 	local name = player:get_player_name()
 	if fields.accept then
 		if minetest.check_player_privs(name, {shout=true}) then
-			minetest.chat_send_player(name, "Thanks for accepting the rules, you now are able to interact with things.")
-			minetest.chat_send_player(name, "Happy building!")
-			minetest.chat_send_player(name, "For more information please type /news")
+			minetest.chat_send_player(name, "Danke für die Besätigung der Nutzungsbedingungen, du bekommst jetzt Baurechte.")
+			minetest.chat_send_player(name, "Viel Spaß beim Bauen.")
 			local privs = minetest.get_player_privs(name)
 			privs.interact = true
 			minetest.set_player_privs(name, privs)
 		end
 		return
 	elseif fields.decline then
-		minetest.kick_player(name, "Bye then! You have to agree to the rules to play on the server (please rejoin if you have a change of mind).")
+		minetest.chat_send_player(name, "Du kannst dich umschauen, aber nicht aktiv bauen. Wenn du es dir anders überlegt hast, schreibe noch einmal /nutzungsbedingungen in den Chat")
 		return
 	end
 end)
 
-minetest.register_chatcommand("rules",{
+minetest.register_chatcommand("nutzungsbedingungen",{
 	params = "",
-	description = "Shows the server rules",
+	description = "Zeigt die Nutzungsbedingungen des Servers",
 	privs = {shout=true},
 	func = function (name,params)
 	local player = minetest.get_player_by_name(name)
