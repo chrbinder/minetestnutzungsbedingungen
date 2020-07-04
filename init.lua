a7e9fdc02610fb135ce9f059a9c69b3feaa517b6
Elias Fleckenstein <54945686+EliasFleckenstein03@users.noreply.github.com>
2020-07-04 08:14:45 +0200

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,7 +1,9 @@
 -- Based on https://github.com/ChaosWormz/mt_terms_of_use
 -- Based on https://github.com/davisonio/craig-server_game/blob/df8beb15e6b02ab6dd22f94349453c51819238c4/mods/_misc/rules.lua
 
-local RULES = [[
+local E = minetest.formspec_escape
+
+local rules = [[
 Nutzungsbedingungen
 1) Ich bin damit einverstanden, dass Daten, die notwendig sind für das Spiel, auf dem Server gespeichert werden (Gebaute und abgebaute Elemente sowie Äußerungen im Chat)
 2) Ich werde nichts mutwillig zerstören, was andere gebaut haben.
@@ -11,12 +13,13 @@ Auf JA klicken, um Baurechte zu erhalten.
 Danke 
 ]]
 
-local function make_formspec()
-	local size = { "size[10,8]" }
-	table.insert(size, "textarea[0.5,0.5;9.5,8;TOS;Das sind die Nutzungsbedingungen. Bitte klicken, um sie zu bestätigen;"..RULES.."]")
-	table.insert(size, "button_exit[6,7.4;1.5,0.5;accept;JA]")
-	table.insert(size, "button_exit[7.5,7.4;1.5,0.5;decline;NEIN]")
-	return table.concat(size)
+local formspec = "size[10,8]"
+	.. "textarea[0.5,0.5;9.5,8;TOS;" .. E("Das sind die Nutzungsbedingungen. Bitte klicken, um sie zu bestätigen") .. ";" .. E(rules) .. "]")
+	.. "button_exit[6,7.4;1.5,0.5;accept;JA]"
+	.. "button_exit[7.5,7.4;1.5,0.5;decline;NEIN]"
+
+local function show_formspec(name)
+	minetest.show_formspec(name, "rules", make_formspec())
 end
 
 minetest.register_on_player_receive_fields(function(player, formname, fields)
@@ -37,14 +40,15 @@ minetest.register_on_player_receive_fields(function(player, formname, fields)
 	end
 end)
 
-minetest.register_chatcommand("nutzungsbedingungen",{
-	params = "",
+minetest.register_chatcommand("nutzungsbedingungen", {
 	description = "Zeigt die Nutzungsbedingungen des Servers",
-	privs = {shout=true},
-	func = function (name,params)
-	local player = minetest.get_player_by_name(name)
-		minetest.after(1, function()
-			minetest.show_formspec(name, "rules", make_formspec())
-		end)
-	end
+	privs = {shout = true},
+	func = show_formspec
 })
+
+minetest.register_on_joinplayer(function(player)
+	local name = player:get_player_name()
+	if not minetest.check_player_privs(name, {interact = true}) then
+		show_formspec()
+	end
+end)
